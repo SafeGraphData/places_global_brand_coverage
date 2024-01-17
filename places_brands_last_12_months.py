b914e92a97ca095c5882d638967e22ac13c15487
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-17 14:56:38 -0500

    add brand counts over time plot

--- places_brands_last_12_months.py
+++ places_brands_last_12_months.py
@@ -55,3 +55,51 @@ brands_by_country = alt.Chart(brands_by_country_df).mark_bar().encode(
 )
 
 st.altair_chart(brands_by_country, use_container_width=True)
+
+#### Overall Brands Last 12 Months ####
+global_places_df = read_from_gsheets("Global Places")
+global_places_df = global_places_df[["Release month", "Country", "Distinct brands"]]
+
+for i, value in enumerate(global_places_df['Release month']):
+    try:
+        global_places_df.loc[i, 'Release month'] = pd.to_datetime(value, format='%b %Y').strftime('%Y-%m')
+    except ValueError:
+        global_places_df.loc[i, 'Release month'] = pd.to_datetime(value, format='%B %Y').strftime('%Y-%m')
+
+start_date_str = (datetime.now() - timedelta(days=365)).strftime("%Y-%m")
+
+global_places_df["Release month"] = pd.to_datetime(global_places_df["Release month"])
+overall_brands_last_12_df = global_places_df[
+    (global_places_df["Release month"] >= start_date_str) & (global_places_df["Release month"] <= datetime.now()) &
+    (global_places_df["Country"] == "Grand Total")
+]
+overall_brands_last_12_df["Release month"] = overall_brands_last_12_df["Release month"].dt.strftime("%Y-%m")
+overall_brands_last_12_df["Distinct brands"] = pd.to_numeric(overall_brands_last_12_df["Distinct brands"])
+overall_brands_last_12_df = overall_brands_last_12_df.rename(columns={"Distinct brands": "Distinct brands - overall"})
+
+# st.dataframe(overall_brands_last_12_df)
+
+y_min = overall_brands_last_12_df['Distinct brands - overall'].min()
+y_max = overall_brands_last_12_df['Distinct brands - overall'].max()
+y_range = [y_min, y_max]
+
+overall_brands_last_12 = alt.Chart(overall_brands_last_12_df).mark_line().encode(
+    x='Release month',
+    y=alt.Y('Distinct brands - overall', scale=alt.Scale(domain=y_range)),
+    tooltip=[alt.Tooltip('Release month'),
+             alt.Tooltip('Country'),
+             alt.Tooltip('Distinct brands - overall', format=',')]
+).properties(
+    width=800,
+    height=400,
+    title=alt.TitleParams(
+        text='Distinct Brand Count Overall - Last 12 months',
+        fontSize=18
+    )
+).configure_axisX(
+    title=None,
+    labelAngle=45
+)
+
+st.altair_chart(overall_brands_last_12,use_container_width=True)
+
