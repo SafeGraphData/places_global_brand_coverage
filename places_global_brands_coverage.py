dfdc112dff95660ccd236ec023dbf28b19067858
ilanrich <54144308+ilanrich@users.noreply.github.com>
2024-01-23 16:09:31 -0500

    fix styling bug for top 1000 brands

--- places_global_brands_coverage.py
+++ places_global_brands_coverage.py
@@ -18,10 +18,15 @@ top_1000_brands_df = (
     .sort_values("primary_brand", ascending=True)
     .reset_index(drop=True)
     .rename(columns={"primary_brand": "Brand name", "naics_code": "NAICS Code", "safegraph_category": "SafeGraph Category", "country_code_list": "Country Code", "country_name_list": "Country Name List"})
-    .style.apply(lambda x: ['background-color: #D7E8ED' if i % 2 == 0 else '' for i in range(len(x))], axis=0)
+    #.style.apply(lambda x: ['background-color: #D7E8ED' if i % 2 == 0 else '' for i in range(len(x))], axis=0)
 )
 
 top_1000_brands_df =  top_1000_brands_df[[x.isnumeric() for x in top_1000_brands_df['NAICS Code']]].reset_index()
+
+top_1000_brands_styled = (
+    top_1000_brands_df
+    .style.apply(lambda x: ['background-color: #D7E8ED' if i % 2 == 0 else '' for i in range(len(x))], axis=0)
+)
 
 brands_by_country_df = (
     read_from_gsheets("Countries")
@@ -42,7 +47,7 @@ brands_by_country_df_styled = (
 brand_tab1, brand_tab2 = st.tabs(["Top 1,000 Brands", "Brand Count By Country"])
 
 with brand_tab1:
-    st.dataframe(top_1000_brands_df, use_container_width=True)
+    st.dataframe(top_1000_brands_styled, use_container_width=True)
 
 with brand_tab2:
     st.dataframe(brands_by_country_df_styled, use_container_width=True)
