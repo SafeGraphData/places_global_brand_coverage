87177c0870c00246335b07f3b6886c1da1746111
ilanrich <54144308+ilanrich@users.noreply.github.com>
2026-01-27 18:31:35 +0100

    Keep-alive commit - 2026-01-27 17:31:35.155967

--- places_global_brands_coverage.py
+++ places_global_brands_coverage.py
@@ -734,3 +734,4 @@ st.markdown(css, unsafe_allow_html=True)
 # Keep-alive comment: 2026-01-26 11:31:29.489089
 # Keep-alive comment: 2026-01-26 21:31:32.905735
 # Keep-alive comment: 2026-01-27 07:31:30.990629
+# Keep-alive comment: 2026-01-27 17:31:34.508443
