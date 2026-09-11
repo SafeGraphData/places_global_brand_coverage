26259a00dcdf2faa6cf0e4dc58bd7792595de773
sgclarissa <148800809+safegraph-clarissa@users.noreply.github.com>
2026-09-11 14:32:06 -0500

    Keep-alive commit - 2026-09-11 19:32:06.494883

--- places_global_brands_coverage.py
+++ places_global_brands_coverage.py
@@ -1260,3 +1260,4 @@ st.markdown(css, unsafe_allow_html=True)
 # Keep-alive comment: 2026-09-10 13:33:09.909925
 # Keep-alive comment: 2026-09-10 23:32:09.951983
 # Keep-alive comment: 2026-09-11 09:32:06.608032
+# Keep-alive comment: 2026-09-11 19:32:05.498082
